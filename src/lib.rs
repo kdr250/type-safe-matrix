83b609dcfd45aa126e1fc806e5d57eef36c1c098
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-04-24 17:11:24 +0900

    行列の加算を実装

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use std::ops::Add;
+use std::ops::AddAssign;
+
+#[derive(Debug, PartialEq)]
+pub struct Matrix2D<const N: usize, const M: usize> {
+    pub elements: [[f32; M]; N],
+}
+
+impl<const N: usize, const M: usize> Matrix2D<N, M> {
+    pub fn new(elements: [[f32; M]; N]) -> Self {
+        Matrix2D { elements }
+    }
+}
+
+impl<const N: usize, const M: usize> Add for Matrix2D<N, M> {
+    type Output = Matrix2D<N, M>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut elements = [[0.0; M]; N];
+        for n in 0..N {
+            for m in 0..M {
+                elements[n][m] = self.elements[n][m] + rhs.elements[n][m];
+            }
+        }
+        return Matrix2D { elements };
+    }
+}
+
+impl<const N: usize, const M: usize> AddAssign for Matrix2D<N, M> {
+    fn add_assign(&mut self, rhs: Self) {
+        for n in 0..N {
+            for m in 0..M {
+                self.elements[n][m] += rhs.elements[n][m];
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Matrix2D;
+
+    #[test]
+    fn add_test() {
+        let expected: Matrix2D<2, 2> = Matrix2D::new([[3.0, 10.0], [10.0, -8.0]]);
+
+        let a: Matrix2D<2, 2> = Matrix2D::new([[3.0, 7.0], [6.0, -4.0]]);
+        let b: Matrix2D<2, 2> = Matrix2D::new([[0.0, 3.0], [4.0, -4.0]]);
+        let actual: Matrix2D<2, 2> = a + b;
+
+        // compilation error
+        // let c: Matrix2D<2, 3> = Matrix2D::new([[0.0; 3], [0.0; 3]]);
+        // a + c;
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn add_asign_test() {
+        let expected: Matrix2D<2, 2> = Matrix2D::new([[3.0, 10.0], [10.0, -8.0]]);
+
+        let mut a: Matrix2D<2, 2> = Matrix2D::new([[3.0, 7.0], [6.0, -4.0]]);
+        let b: Matrix2D<2, 2> = Matrix2D::new([[0.0, 3.0], [4.0, -4.0]]);
+        a += b;
+
+        // compilation error
+        // let c: Matrix2D<2, 3> = Matrix2D::new([[0.0; 3], [0.0; 3]]);
+        // a += c;
+
+        assert_eq!(a, expected);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use type_safe_matrix::Matrix2D;
+
 fn main() {
-    println!("Hello, world!");
+    let a: Matrix2D<2, 2> = Matrix2D::new([[3.0, 7.0], [6.0, -4.0]]);
+    println!("{:?}", a);
 }
